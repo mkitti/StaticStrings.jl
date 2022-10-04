183fbb737b80406aa1bc16985d9a076fac146adc
Mark Kittisopikul <markkitt@gmail.com>
2022-10-04 07:34:24 -0400

    Add macro tests

---
+++ test/macros.jl
@@ -0,0 +1,24 @@
+using StaticStrings
+using Test
+
+Static"Lorem ipsum dolor sit amet "
+Static"Lorem ipsum dolor sit amet"31
+Short"Lorem ipsum dolor sit amet "
+Short"Lorem ipsum dolor sit amet"31
+Long"Lorem ipsum dolor sit amet "
+Long"Lorem ipsum dolor sit amet"31
+CStatic"Lorem ipsum dolor sit amet\0"
+CStatic"Lorem ipsum dolor sit amet"31
+Padded"Lorem ipsum dolor sit amet "31
+
+@testset "Macros" begin
+    @test @allocated(Static"Lorem ipsum dolor sit amet") == 0
+    @test @allocated(Static"Lorem ipsum dolor sit amet"31) == 0
+    @test @allocated(Short"Lorem ipsum dolor sit amet") == 0
+    @test @allocated(Short"Lorem ipsum dolor sit amet"31) == 0
+    @test @allocated(Long"Lorem ipsum dolor sit amet") == 0
+    @test @allocated(Long"Lorem ipsum dolor sit amet"31) == 0
+    @test @allocated(CStatic"Lorem ipsum dolor sit amet") == 0
+    @test @allocated(CStatic"Lorem ipsum dolor sit amet"31) == 0
+    @test @allocated(Padded"Lorem ipsum dolor sit amet "31) == 0
+end
