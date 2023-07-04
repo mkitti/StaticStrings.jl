b04f8755a8f5825a14d96e3b32f56b8359826b7b
Mark Kittisopikul <kittisopikulm@janelia.hhmi.org>
2023-07-03 20:52:02 -0400

    Define ncodeunits for AbstractStaticString{N}

--- src/AbstractStaticString.jl
+++ src/AbstractStaticString.jl
@@ -19,6 +19,7 @@ Base.codeunit(::AbstractStaticString) = UInt8
 Base.@propagate_inbounds Base.codeunit(s::AbstractStaticString, i::Int) = codeunits(s)[i]
 @inline Base.codeunits(s::AbstractStaticString) = s.data
 @inline Base.ncodeunits(::AbstractStaticString{N}) where N = N
+@inline Base.ncodeunits(::Type{<: AbstractStaticString{N}}) where N = N
 Base.widen(::AbstractStaticString) = String
 Base.promote_rule(::Type{T}, ::Type{String}) where {T <: AbstractStaticString} = String
 
