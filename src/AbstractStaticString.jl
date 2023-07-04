eec3c848b770c81feddae588666dfb9de71560ad
Mark Kittisopikul <mkitti@users.noreply.github.com>
2023-07-03 21:18:17 -0400

    Mkitti/fix_write_return (#18)
    * Fix write return type
    
    * Define ncodeunits for AbstractStaticString{N}
    
    * Add AbstractStaticString tests

--- src/AbstractStaticString.jl
+++ src/AbstractStaticString.jl
@@ -19,7 +19,9 @@ Base.codeunit(::AbstractStaticString) = UInt8
 Base.@propagate_inbounds Base.codeunit(s::AbstractStaticString, i::Int) = codeunits(s)[i]
 @inline Base.codeunits(s::AbstractStaticString) = s.data
 @inline Base.ncodeunits(::AbstractStaticString{N}) where N = N
-Base.widen(::AbstractStaticString) = String
+@inline Base.ncodeunits(::Type{<: AbstractStaticString{N}}) where N = N
+Base.widen(::Type{<: AbstractStaticString}) = String
+Base.widen(s::AbstractStaticString) = String(s)
 Base.promote_rule(::Type{T}, ::Type{String}) where {T <: AbstractStaticString} = String
 
 
