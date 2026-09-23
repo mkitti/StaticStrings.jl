44e1f4942290fe00b597e532b0802d5e7b89a318
Patrick Häcker <152268010+PatrickHaecker@users.noreply.github.com>
2026-09-23 09:52:04 +0200

    Add `data` back as deprecated API
    Co-authored-by: Mark Kittisopikul <mkitti@users.noreply.github.com>

--- src/AbstractStaticString.jl
+++ src/AbstractStaticString.jl
@@ -11,6 +11,8 @@ abstract type AbstractStaticString{N} <: AbstractString end
 (ASS::Type{<:AbstractStaticString})(data::NTuple{N,Int8}) where N = ASS{N}(UInt8.(data))
 (ASS::Type{<:AbstractStaticString{N}})(data::NTuple{N,Int8}) where N = ASS(UInt8.(data))
 
+Base.@deprecate data(s::AbstractStaticString) Tuple(s) false
+
 Base.codeunit(::AbstractStaticString) = UInt8
 Base.@propagate_inbounds Base.codeunit(s::AbstractStaticString, i::Int) = codeunits(s)[i]
 @inline Base.codeunits(s::AbstractStaticString) = s.data
